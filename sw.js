93d6cef82d9a7ab7fd53a6dd7ef234d3d9a0af9c
daniell00gomdz9-svg <daniell.00gomdz.9@gmail.com>
2026-03-02 07:18:52 -0600

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'voyager-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js'
+];
+
+// Instalar y cachear recursos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Servir desde caché si no hay red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
